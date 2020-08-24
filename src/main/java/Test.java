1eea1c46d4530af3d4f772599c50c50ebfbab513
ls <ls@qq.com>
2020-08-24 15:14:20 +0800

    经理提交

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -6,5 +6,8 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }
